3e7436278587be82e2e46d9152a3df602908bdf1
Luis Garelli <luiscarlosgarelli@gmail.com>
2023-01-27 16:19:00 +0100

    added 2nd test

--- src/test/java/RestAssuredTests.java
+++ src/test/java/RestAssuredTests.java
@@ -1,14 +1,31 @@
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class RestAssuredTests {
+
+  String url = "https://demo.guru99.com/V4/sinkministatement.php";
+
   @Test
   public void testAPI() {
+
+
+
+    int statusCode = given().
+        queryParam("CUSTOMER_ID", "68195").
+        queryParam("PASSWORD", "1234!").
+        queryParam("Account_No", "1").
     when()
-        .get("https://demo.guru99.com/V4/sinkministatement.php?CUSTOMER_ID=68195&PASSWORD=1234!&Account_No=1")
-    .then()
-        .log().body();
+        .get(url).getStatusCode();
+
+    System.out.println("Status code est: " + statusCode);
+    assertEquals(200, statusCode);
+  }
+
+  @Test
+  public void testApi2() {
+    when().get(url).then().assertThat().statusCode(200);
   }
 }
